056eab1ca0da051ce1bc894c0c8910555b518d2e
Fernando <isquierdo.ads@gmail.com>
2021-05-27 18:17:22 -0300

    Add DataBase files

---
+++ dbTeamMembers.js
@@ -0,0 +1,31 @@
+let teamMembers = [
+  {
+    id: 1,
+    nome: "Fernando",
+    age: 32,
+  },
+  {
+    id: 2,
+    nome: "Bob",
+    age: 62,
+  },
+  {
+    id: 3,
+    nome: "Marley",
+    age: 12,
+  },
+  {
+    id: 4,
+    nome: "Anastácia",
+    age: 23,
+  },
+  {
+    id: 5,
+    nome: "Ericaum",
+    age: 58,
+  },
+];
+
+
+
+module.exports = teamMembers;
