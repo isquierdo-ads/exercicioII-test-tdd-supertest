0a9c08207df3935848e3db4143ff71076f55a181
Fernando <isquierdo.ads@gmail.com>
2021-05-29 22:48:32 -0300

    Final Version

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-const teamMembers = require("./dbTeamMembers");
+let teamMembers = require("./dbTeamMembers");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-
 
 app.get("/teamMembers", (req, res) => {
   res.send(teamMembers);
 });
 
 app.post("/teamMembers", (req, res) => {
-  const { name, age } = request.body;
+  const { id, name, age } = req.body;
   const newMember = {
+    id,
     name,
     age,
   };
 
   teamMembers.push(newMember);
-  response.status(201).json(newMember);
+  res.status(201).json(newMember);
+});
+
+app.post("/teamMembers", (req, res) => {
+  const { id, name, age } = req.body;
+  const newMember = {
+    id,
+    name,
+    age,
+  };
+  teamMembers.push(newMember);
+  res.status(201).json(newMember);
+});
+
+app.put("/teamMembers/:id", (req, res) => {
+  const { id } = req.params;
+
+  const { age } = req.body;
+
+  const p = teamMembers.find((v) => v.id == id);
+
+  if (p && typeof age == "number") {
+    p.age = age;
+    res.json(p);
+  } else {
+    res.status(400).send({ Error: "Insira uma idade válida" });
+  }
+});
+
+app.delete("/teamMembers/:id", (req, res) => {
+  const { id } = req.params;
+  const index = teamMembers.findIndex((v) => v.id == id);
+
+  if (index == -1) {
+    res.status(400).send("Usuário não encontrado");
+  } else {
+    teamMembers = teamMembers.filter((v) => v.id != id);
+    res.status(204).send({ ok: "Usuário deletado com sucesso" });
+  }
 });
 
 module.exports = app;
--- teamMembers.test.js
+++ teamMembers.test.js
@@ -8,14 +8,30 @@ test("Should list team members exactly", async () => {
   expect(responseGet.body).toMatchObject(teamMembers);
 });
 
-
-
-
-
-
-
-test("Should list team members exactly", async () => {
-  const responseGet = await request(app).post("/teamMembers");
-
-  expect
+test("Should be possible to add another member", async () => {
+  const tmLen = teamMembers.length;
+  console.log(tmLen);
+  const response = await request(app)
+    .post("/teamMembers")
+    .send(teamMembers[tmLen + 1]);
+  expect(response.body).toMatchObject({
+    ...teamMembers[tmLen + 1],
+  });
 });
+
+test("Status code from 'post' should be 201", async () => {
+  await request(app).post("/teamMembers").send(teamMembers[0]);
+
+  expect(201);
+});
+
+test("Shouldn't be possible to update a non-existent member", async () => {
+  await request(app).put("/teamMembers/-1").expect(400);
+});
+
+
+test("Shouldn't be possible to delete a non-existent member", async () => {
+  await request(app).del("/teamMembers/-1").expect(400);
+});
+
+
